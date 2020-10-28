6b932d15fae30333b59b821dc6593ba90085e740
bogdan71 <boguslaw@gmail.com>
2020-10-28 13:42:45 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue; 
+
+namespace QueueAppSample
+{
+    class Program
+    {
+        private const string ConnectionString = "DefaultEndpointsProtocol=https;EndpointSuffix=core.windows.net;AccountName=bbstorageaccount1971;AccountKey=FvIoV40hD05DkZwtMlzxpztGFaa8gXbxRsKhRHxNjJwTEpSQ3NPWFq5rO0/A0y6e+a+0W87ahbDqXalnZDPCIQ==";
+
+        static async Task Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                string value = String.Join(" ", args);
+                await SendArticleAsync(value);
+                Console.WriteLine($"Sent: {value}");
+            }
+            else
+            {
+                string value = await ReceiveArticleAsync();
+                Console.WriteLine($"Received {value}");
+            }
+        }
+
+        static async Task SendArticleAsync(string newsMessage)
+        {
+            CloudQueue queue = GetQueue();
+            bool createdQueue = await queue.CreateIfNotExistsAsync();
+            if (createdQueue)
+            {
+                Console.WriteLine("The queue of news articles was created.");
+            }
+
+            CloudQueueMessage articleMessage = new CloudQueueMessage(newsMessage);
+            await queue.AddMessageAsync(articleMessage);
+        }
+
+        static async Task<string> ReceiveArticleAsync()
+        {
+            CloudQueue queue = GetQueue();
+            bool exists = await queue.ExistsAsync();
+            if (exists)
+            {
+                CloudQueueMessage retrievedArticle = await queue.GetMessageAsync();
+                if (retrievedArticle != null)
+                {
+                    string newsMessage = retrievedArticle.AsString;
+                    await queue.DeleteMessageAsync(retrievedArticle);
+                    return newsMessage;
+                }
+            }
+
+            return "<queue empty or not created>";
+        }
+
+        static CloudQueue GetQueue()
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConnectionString);
+
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+            return queueClient.GetQueueReference("newsqueue");
+        }
+    }
+}
